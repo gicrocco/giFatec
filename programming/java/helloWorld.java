04af1f6152a30fd1faede9f918a0716a38f6cf6d
gicrocco <croccogiovanni.08@gmail.com>
2021-10-13 12:09:12 -0300

    .

---
+++ programming/java/helloWorld.java
@@ -0,0 +1,10 @@
+public class class_one{
+
+	public static void main(String[] args){
+
+		System.out.println("Hello World!");
+		
+
+	}
+
+}
---
+++ programming/java/variables.java
@@ -0,0 +1,13 @@
+public class class_one{
+	import java.util;
+	public static void main(String[] args){
+
+			Scanner keyboard = new Scanner(System.in);
+
+			System.out.println("Type a number ");
+			double numberInsert = next.Double();
+
+
+	}
+
+}
