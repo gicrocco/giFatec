eeb1ca6f4003acc2baa7b8b25457b1896b732bc5
gicrocco <croccogiovanni.08@gmail.com>
2021-10-14 13:25:13 -0300

    ok

---
+++ programming/python3/testII.py
@@ -0,0 +1,13 @@
+
+name = "Giovanni"
+print(name.upper())
+def welcome_user():
+    return "welcome!"
+
+number = 0
+if number > 0:
+    print(f"number {number} is positive")
+elif number == 0:
+    print(number)
+else:
+    print(f"{number} is negative")
---
+++ programming/python3/testIII.py
@@ -0,0 +1,10 @@
+
+number = 10
+
+message = "positive" if number > 0 else "0 or negative"
+print(message)
+
+names = ["James", "Mary", "Karen", "Julian"]
+#names.sort()
+#names.reverse()
+print(names)
