3fe815136cec234620a8eb1adfb5a31687fd139c
gicrocco <croccogiovanni.08@gmail.com>
2021-10-14 20:09:52 -0300

    ok

---
+++ programming/ruby/ruby.rb
@@ -0,0 +1,22 @@
+#beginning
+
+print "Enter a value: "
+first_num = gets.to_i
+
+print "Enter another value: "
+
+second_num = gets.to_i
+
+puts first_num.to_s + "+" + second_num.to_s + "=" +
+(first_num + second_num).to_s
+
+puts "6+4 = " + (6+4).to_s
+puts "6-4 = " + (6-4).to_s
+puts "6%4 = " + (6%4).to_s
+puts "6/4 = " + (6/4).to_s
+puts "6*4 = " + (6*4).to_s
+num_one = 1.000
+num99 = 0.999
+puts num_one.to_s + "-" + num99.to_s + "="+(num_one - num99).to_s
+big_float = 1.1234578901234
+puts (big_float + 0.0000000000005).to_s
